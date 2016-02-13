e4d65df6211884820c01f346ee9de9a7dbcf0e93
hs.yang <yhs@hsyangs-iMac.local>
2016-02-13 00:02:57 -0800

    parse chinese title for gogoup

--- src/you_get/extractors/gogoup.py
+++ src/you_get/extractors/gogoup.py
@@ -17,12 +17,16 @@ def gogoup_download(url, output_dir='.', merge=True, info_only=False ,**kwargs):
     if re.match(r'http:\/\/www\.gogoup\.com\/play\?festId=\d+', url):
         cookies = pycookiecheat.chrome_cookies(url)
         sess = requests.Session()
-        video_page = str(sess.get(url, cookies = cookies).content)
+        # Decode byte string into proper codec, here is 'utf-8'.
+        video_page = sess.get(url, cookies = cookies).content.decode('utf-8')
         # Get uu, vu
         uu = re.search('\"uu\":"[a-f0-9]+"', video_page).group().split('"')[-2]
         vu = re.search('\"vu\":"[a-f0-9]+"', video_page).group().split('"')[-2]
-        print('Got vu: %s, uu: %s' % (vu, uu))
-        title = "gogoup-%s" % vu
+        title = "gogoup-"
+        title = title + re.search('\<span class=\"lesson-name\"\>(.*?)\<\/span\>', video_page).group(1)
+        title = title + '-' + re.search('\<span class=\"part-name\"\>(.*?)\<\/span\>', video_page).group(1)
+        title = title.replace(' ', '')
+        print('Got vu: %s, uu: %s, title: %s' % (vu, uu, title))
         letvcloud_download_by_vu(vu, uu, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
     else:
         print('The page url is not supported. URL: %s' % url)
